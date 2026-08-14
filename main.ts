16d882ac11d99647391bcbfd4582f13b82705650
HaruS0610 <h85034588@gmail.com>
2026-08-15 08:07:44 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,11 @@
-/**
- * External Speaker
- */
 //% color="#9B59B6" icon="\uf028" block="外付けスピーカー"
 namespace ExternalSpeaker {
 
-    /**
-     * P0に音声を出力
-     */
-    //% block="音楽の出力を P0 にする"
+    //% block="音楽の出力ピンを %pin に設定"
+    //% pin.defl=AnalogPin.P0
     //% weight=100
-    export function setP0(): void {
-        pins.setAudioPin(AnalogPin.P0)
-        pins.setAudioPinEnabled(true)
-    }
-
-    /**
-     * P1に音声を出力
-     */
-    //% block="音楽の出力を P1 にする"
-    //% weight=90
-    export function setP1(): void {
-        pins.setAudioPin(AnalogPin.P1)
-        pins.setAudioPinEnabled(true)
-    }
-
-    /**
-     * P2に音声を出力
-     */
-    //% block="音楽の出力を P2 にする"
-    //% weight=80
-    export function setP2(): void {
-        pins.setAudioPin(AnalogPin.P2)
+    export function setMusicPin(pin: AnalogPin): void {
+        pins.setAudioPin(pin)
         pins.setAudioPinEnabled(true)
     }
 }

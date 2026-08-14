5e42cf7a17e821d037b4af5b64abf1e486e9e10f
HaruS0610 <h85034588@gmail.com>
2026-08-15 08:37:47 +0900

    Update device.d.ts

--- device.d.ts
+++ device.d.ts
@@ -1,26 +1,12 @@
-/**
- * Retro Arcade N3 audio pins
- */
-
 declare namespace pins {
-
-    /**
-     * Audio output P0
-     */
     //% fixedInstance
     //% shim=pxt::getPinCfg(CFG_PIN_AUDIO_P0)
     const AUDIO_P0: AnalogOutPin
 
-    /**
-     * Audio output P1
-     */
     //% fixedInstance
     //% shim=pxt::getPinCfg(CFG_PIN_AUDIO_P1)
     const AUDIO_P1: AnalogOutPin
 
-    /**
-     * Audio output P2
-     */
     //% fixedInstance
     //% shim=pxt::getPinCfg(CFG_PIN_AUDIO_P2)
     const AUDIO_P2: AnalogOutPin

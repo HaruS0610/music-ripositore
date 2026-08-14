94e3bbfbe419d34f1825f1b4f9a749e4058404b3
HaruS0610 <h85034588@gmail.com>
2026-08-15 07:49:54 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 /**
- * 外付けスピーカー
+ * External Speaker
  */
 //% color="#9B59B6" icon="\uf028" block="外付けスピーカー"
 namespace ExternalSpeaker {
 
     /**
-     * 音楽の出力ピンを設定
-     * @param pin 音楽を出力するピン
+     * 音楽の出力ピンを設定する
      */
     //% block="音楽の出力ピンを %pin に設定"
-    //% pin.defl=AnalogPin.P0
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.columns=3
+    //% pin.fieldOptions.tooltips="true"
     //% weight=100
-    //% group="スピーカー"
     export function setMusicPin(pin: AnalogPin): void {
         pins.setAudioPin(pin)
         pins.setAudioPinEnabled(true)
@@ -22,7 +22,6 @@ namespace ExternalSpeaker {
      */
     //% block="外付けスピーカーを有効にする"
     //% weight=90
-    //% group="スピーカー"
     export function enable(): void {
         pins.setAudioPinEnabled(true)
     }
@@ -32,18 +31,37 @@ namespace ExternalSpeaker {
      */
     //% block="外付けスピーカーを無効にする"
     //% weight=80
-    //% group="スピーカー"
     export function disable(): void {
         pins.setAudioPinEnabled(false)
     }
 
     /**
-     * 音楽出力ピンを取得
+     * 音楽の出力ピンをP0に設定
      */
-    //% block="音楽の出力ピン"
+    //% block="音楽の出力を P0 にする"
     //% weight=70
-    //% group="スピーカー"
-    export function musicPin(): AnalogPin {
-        return pins.getAudioPin()
+    export function setP0(): void {
+        pins.setAudioPin(AnalogPin.P0)
+        pins.setAudioPinEnabled(true)
+    }
+
+    /**
+     * 音楽の出力をP1に設定
+     */
+    //% block="音楽の出力を P1 にする"
+    //% weight=60
+    export function setP1(): void {
+        pins.setAudioPin(AnalogPin.P1)
+        pins.setAudioPinEnabled(true)
+    }
+
+    /**
+     * 音楽の出力をP2に設定
+     */
+    //% block="音楽の出力を P2 にする"
+    //% weight=50
+    export function setP2(): void {
+        pins.setAudioPin(AnalogPin.P2)
+        pins.setAudioPinEnabled(true)
     }
 }

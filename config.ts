e431e688ecdab5027e10378a3ef713cfbe215172
HaruS0610 <h85034588@gmail.com>
2026-08-15 08:41:13 +0900

    Delete config.ts

--- config.ts
+++
@@ -1,7 +0,0 @@
-namespace config {
-    export const PIN_AUDIO_P0 = DAL.P0
-    export const PIN_AUDIO_P1 = DAL.P1
-    export const PIN_AUDIO_P2 = DAL.P2
-
-    export const PIN_SPEAKER = PIN_AUDIO_P0
-}

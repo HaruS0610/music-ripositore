3c76dd2b9820d7d208aa3f41d0d674174dd7c965
HaruS0610 <h85034588@gmail.com>
2026-08-15 09:01:27 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,18 @@
-//% color="#8E44AD" icon="\uf028" block="Retro Arcade スピーカー"
+//% color="#8E44AD" block="Retro Arcade スピーカー"
 namespace RetroArcadeSpeaker {
 
-    //% block="音声出力を P0 に設定"
+    //% block="P0 を選択"
     //% weight=100
     export function setP0(): void {
-        music.setVolume(255)
     }
 
-    //% block="音声出力を P1 に設定"
+    //% block="P1 を選択"
     //% weight=90
     export function setP1(): void {
-        music.setVolume(255)
     }
 
-    //% block="音声出力を P2 に設定"
+    //% block="P2 を選択"
     //% weight=80
     export function setP2(): void {
-        music.setVolume(255)
     }
 }
